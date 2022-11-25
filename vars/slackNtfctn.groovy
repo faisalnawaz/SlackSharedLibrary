e883a49eb2f17cf199516c4141c18af34292f9d4
faisalnawaz <42169344+faisalnawaz@users.noreply.github.com>
2022-11-25 05:50:55 +0500

    Update slackNtfctn.groovy

--- vars/slackNtfctn.groovy
+++ vars/slackNtfctn.groovy
@@ -1,4 +1,4 @@
 #!/usr/bin/env groovy
 def call(String clr = "#439FE0", String msg = "Build Successful") {
-  slackSend color:${clr}, message:${msg} 
+  slackSend color:"${clr}", message:"${msg}" 
 }

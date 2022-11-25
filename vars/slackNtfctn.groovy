3625411c6da2cb98b8fcb54bbf594ac41f2ba465
faisalnawaz <42169344+faisalnawaz@users.noreply.github.com>
2022-11-25 05:42:11 +0500

    Update slackNtfctn.groovy

--- vars/slackNtfctn.groovy
+++ vars/slackNtfctn.groovy
@@ -1,4 +1,4 @@
 #!/usr/bin/env groovy
 def infoShr(String cmnd = "slackSend", String clr = "#439FE0", String msg = "Build Successful") {
-  echo "${cmnd} color: ${clr}, message: ${msg}"
+  "${cmnd} color: ${clr}, message: ${msg}"
 }

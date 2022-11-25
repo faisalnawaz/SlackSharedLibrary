0cb42cc90c557e249137a5ac3a55cbc59a61980b
faisalnawaz <42169344+faisalnawaz@users.noreply.github.com>
2022-11-25 05:13:14 +0500

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,4 +1,4 @@
 #!/usr/bin/env groovy
-def call(String cmnd = "slackSend", String clr = "#439FE0", String msg = "Build Successful") {
-   sh "${cmnd} color: ${clr}, message: ${msg}"
+def call(String name = 'human') {
+  echo "Hello, ${name}."
 }

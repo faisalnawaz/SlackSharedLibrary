25ededb3aeec619c9ffa0291867d410524771a32
faisalnawaz <42169344+faisalnawaz@users.noreply.github.com>
2022-11-25 05:11:25 +0500

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,4 +1,4 @@
 #!/usr/bin/env groovy
-def infoShr(String cmnd = "slackSend", String clr = "#439FE0", String msg = "Build Successful") {
+def call(String cmnd = "slackSend", String clr = "#439FE0", String msg = "Build Successful") {
    sh "${cmnd} color: ${clr}, message: ${msg}"
 }
